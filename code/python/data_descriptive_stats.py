de90b87eea0be0921f8a03cad3fce0601181fb0b
Alvin <alvin.pastore@gmail.com>
2016-12-19 17:23:33 +0000

    minor changes. might become deprecated as planning to use matlab for data statistics

--- code/python/data_descriptive_stats.py
+++ code/python/data_descriptive_stats.py
@@ -2,17 +2,18 @@ from DataReader import DataReader
 
 # load data into subjects objects
 path = '../../data/'
-file_name = 'BarronErev2003.txt'
+file_name = 'BarronErev2003_Thaler_replication.txt'
 
 
 dr = DataReader(path + file_name)
 
-data = dr.read_subject_data()
+problem_data = dr.read_subject_data()
 
-for problem_id, subjects in data.items():
+for problem_id, subjs in problem_data.iteritems():
     print problem_id
-    print subjects
-    print subjects[1].get_trials(1)
-    print subjects[1].get_trials(2)
-    print subjects[1].get_trials(3)
+    for subj_id, subj in subjs.iteritems():
+        print subj_id
+        trials = subj.get_trials()
+        for t in trials:
+            print t
     raw_input()
