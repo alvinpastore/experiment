1927419de9353586f14adac0856eda8479ac40b9
Alvin <alvin.pastore@gmail.com>
2017-02-21 15:08:45 +0000

    Multiple
    - store results in tuples in a multi-level dictionary structure (easier to retrieve later)
    - save results to file function
    - total time print
    - range of alpha and beta extended to lower values

--- code/python/controller.py
+++ code/python/controller.py
@@ -45,13 +45,41 @@ def get_guesspoints_permutations(a, b, g):
     return gp_perms
 
 
+def save_results(file_path, results_filename, results):
+    with open(file_path + results_filename, 'w') as results_file:
+        for problem_id, problem in results.iteritems():
+            print 'problem ', problem_id
+            results_file.write('problem ' + str(problem_id) + '\n')
+            for subj_id, subj in problem.iteritems():
+                print 'subject ', subj_id
+                results_file.write('subject ' + str(subj_id) + '\n')
+                for config, resl in subj.iteritems():
+                    print 'config ', config
+                    print 'params ', resl[0]
+                    print 'MLE ', resl[1]
+                    print
+                    results_file.write(str(config[0]) + ',')   # state-space
+                    results_file.write(str(config[1]) + ',')   # learning rule
+                    results_file.write(str(config[2]) + ',')   # reward function
+                    results_file.write(str(resl[0][0]) + ',')  # alpha
+                    results_file.write(str(resl[0][1]) + ',')  # beta
+                    results_file.write(str(resl[0][2]) + ',')  # gamma
+                    results_file.write(str(resl[1]) + '\n')    # MLE
+
+
 if __name__ == '__main__':
+    t_total = time.time()
     VERBOSE = 0
     MLE_INIT = 9999
 
+    # path and filename
+    file_path = '../../data/'
+    file_name = 'BarronErev2003_Thaler_replication.txt'
+    results_filename = 'test_results.txt'
+
     state_spaces = (constants.STATELESS, constants.FULL_HISTORY, constants.LATEST_OUTCOME)
     learning_rules = (constants.Q_LEARNING, constants.AVG_TRACKING)
-    reward_functions = (constants.IDENTITY, constants.HTAN, constants.PT_VALUE_FUNC)
+    reward_functions = (constants.IDENTITY, constants.TANH, constants.PT_VALUE_FUNC)
     model_configurations = get_modules_permutations(state_spaces, learning_rules, reward_functions)
 
     bounds = ((0.0001, 0.5), (0.00001, 0.5), (0.01, 1))
@@ -59,16 +87,14 @@ if __name__ == '__main__':
     # betas =  ( 0.01,  0.03,  0.1,  0.3,   1,   3,  10, 30)
     # gammas = (0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 0.5, 0.7)
 
-    alphas = (0.001, 0.01, 0.1, 0.3)
-    betas = (0.0001, 0.0003, 0.001, 0.1, 1)
-    gammas = (0.01, 0.2)
+    alphas = (0.0001, 0.001, 0.1, 0.3)
+    betas = (0.00001, 0.001, 0.1)
+    gammas = (0.01, 0.3)
+
     guesspoints_permutations = get_guesspoints_permutations(alphas, betas, gammas)
 
-    # path and filename
-    file_path = '../../data/BarronErev2003_Thaler_replication.txt'
-    print os.listdir('../../data/')
-    data_reader = DataReader(file_path)
     # load data into dictionary {problem: subjects}
+    data_reader = DataReader(file_path + file_name)
     problem_data = data_reader.read_subject_data()
 
     # TODO create data structure to hold results
@@ -77,8 +103,6 @@ if __name__ == '__main__':
     # save for each player the log_lik of each configuration best param set
     # these will be compared in terms of AIC/BIC to determine if the best config is better than the others
     # for example: (two-states vs stateless)
-
-    # TODO improve the htan omega (squash) based on the range of outcomes
 
     # iterate over problems
     for problem_id, subjects in problem_data.iteritems():
@@ -103,6 +127,9 @@ if __name__ == '__main__':
             for configuration in model_configurations:
                 t_config = time.time()
 
+                # placeholder for first result
+                results[problem_id][subj_id][configuration] = ([0, 0, 0], MLE_INIT)
+
                 for guesspoints in guesspoints_permutations:
 
                     # minimise the fitting function
@@ -116,24 +143,17 @@ if __name__ == '__main__':
                         MLE = minimisation_results['fun']
                         params = list(minimisation_results['x'])
                         config = configuration
-                        results[problem_id][subj_id][config] = (params, MLE)
 
-                print '{}: {}'.format(configuration, time.time()-t_config)
+                    # store the results of the configuration if the LL is better than the previous one
+                    if minimisation_results['fun'] < results[problem_id][subj_id][configuration][1]:
+                        results[problem_id][subj_id][configuration] = (minimisation_results['x'], minimisation_results['fun'])
 
-            if MLE < MLE_INIT:
-                print 'MLE ' + str(MLE)
-                print 'params ' + str(params)
-                print 'config ' + str(config)
-            else:
+                print '{} - {} - {}: {}'.format(MLE, configuration, params, time.time()-t_config)
+
+            if MLE >= MLE_INIT:
                 print 'no solution for subject'
             print 'time elapsed for subject {}: {}'.format(subj_id, time.time() - t_subj)
 
-    for problem_id, problem in results.iteritems():
-        print 'problem ', problem_id
-        for subj_id, subj in problem.iteritems():
-            print 'subject ', subj_id
-            for config, resl in subj.iteritems():
-                print 'config ', config
-                print 'mle ', resl[0]
-                print 'params ', resl[1]
-                print
+    save_results(file_path, results_filename, results)
+
+    print 'total time {}'.format(time.time() - t_total)
