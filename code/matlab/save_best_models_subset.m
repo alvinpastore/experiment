e2099e6e868b36dfa7455a73fe11c95f2dd2b11f
Alvin <alvin.pastore@gmail.com>
2017-03-22 11:55:55 +0000

    save_best_models_subset is a separate script

---
+++ code/matlab/save_best_models_subset.m
@@ -0,0 +1,15 @@
+function save_best_models_subset(file_path,file_name,best_multiple_models)
+%SAVE_SINGLE_BEST_MODELS 
+%saves to csv file the single best model for each problem/subject
+
+    fileID = fopen([file_path, 'BEST_MODELS_SUBSET_', file_name],'w');
+    format_spec = '%f,%f,%f,%f,%f,%f,%s,%s,%s\n';
+    [~,n_subjs] = size(best_multiple_models);
+    for subj = 1:n_subjs
+        [n_mods,~] = size(best_multiple_models{1,subj}{1,1});
+        for mod_idx = 1:n_mods
+            fprintf(fileID,'%f,%f,%f,%f,%f,%f,',best_multiple_models{1,subj}{1,1}(mod_idx,:));
+            fprintf(fileID,'%s,%s,%s\n',best_multiple_models{1,subj}{1,2}{mod_idx,:});
+        end
+    end
+    fclose(fileID);
