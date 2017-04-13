12f93c356fccd0f8c849cf757f3112d356143335
Alvin <alvin.pastore@gmail.com>
2017-04-13 11:00:34 +0100

    Plot AIC only (no random and threshold)
    Blue bars for accepted models and red for rejected ones

--- code/matlab/plot_model_results.m
+++ code/matlab/plot_model_results.m
@@ -1,11 +1,10 @@
-function plot_model_results(subj_idx,DEG_OF_FREEDOM,aic,bic,r_AIC,r_BIC)
+function plot_model_results(subj_idx,DEG_OF_FREEDOM,sorted_aic,bic,r_AIC,r_BIC,RND_THRESHOLD,best_aics)
 %PLOT_MODEL_RESULTS 
 %plot results of a problem/subject to single figure, 2 columns AIC/BIC subplots
 % 
     %% PLOT CONSTANTS
     FS = 20;
     LW = 2;
-    BAR_OFFSET = 10;
     
     % colors
     red = [215,25,28]/255;
@@ -17,30 +16,34 @@ function plot_model_results(subj_idx,DEG_OF_FREEDOM,aic,bic,r_AIC,r_BIC)
     fh = figure();
     TITLE = ['Subject ',num2str(subj_idx)];
     
-    subplot(1,2,1);
-    bar(1:length(DEG_OF_FREEDOM),aic,'FaceColor',blue);
+    number_best_models = sum(best_aics);
+    number_total_models = 15;
+    
+    %subplot(1,2,1);
+    bar(1:number_best_models,sorted_aic(1:number_best_models),'FaceColor',blue);
     hold on
-    bar(length(DEG_OF_FREEDOM)+1,r_AIC,'FaceColor',red);
-    plot([0 length(DEG_OF_FREEDOM)+2],[r_AIC-10,r_AIC-10],'Color',red,'LineStyle','--','LineWidth',LW)
+    bar(number_best_models+1:number_total_models,sorted_aic(number_best_models+1:end),'FaceColor',red);
+    %plot([0 length(DEG_OF_FREEDOM)+2],[r_AIC-RND_THRESHOLD,r_AIC-RND_THRESHOLD],'Color',red,'LineStyle','--','LineWidth',LW)
     ylabel('AIC');
     xlabel('Models');
     title(TITLE);
-    axis([0 length(DEG_OF_FREEDOM)+2 min(min(aic),min(bic))-BAR_OFFSET 295]);
+    %axis([0 length(DEG_OF_FREEDOM)+2 min(min(aic),min(bic))-RND_THRESHOLD 295]);   with random bar
+    axis([0 length(DEG_OF_FREEDOM)+1 min(min(sorted_aic),min(bic))-RND_THRESHOLD max(sorted_aic)+5]);
     xticks([]);
     set(gca,'FontSize',FS);
 
-    subplot(1,2,2);
-    bar(1:length(DEG_OF_FREEDOM),bic,'FaceColor',blue);
-    hold on
-    bar(length(DEG_OF_FREEDOM)+1,r_BIC,'FaceColor',red);
-    plot([0 length(DEG_OF_FREEDOM)+2],[r_BIC-10,r_BIC-10],'Color',red,'LineStyle','--','LineWidth',LW)
-    ylabel('BIC');
-    xlabel('Models');
-    title(TITLE);
-    axis([0 length(DEG_OF_FREEDOM)+2 min(min(aic),min(bic))-BAR_OFFSET 295]);
-    xticks([]);
-    set(gca,'FontSize',FS);
-    hold off
+%     subplot(1,2,2);
+%     bar(1:length(DEG_OF_FREEDOM),bic,'FaceColor',blue);
+%     hold on
+%     bar(length(DEG_OF_FREEDOM)+1,r_BIC,'FaceColor',red);
+%     plot([0 length(DEG_OF_FREEDOM)+2],[r_BIC-10,r_BIC-10],'Color',red,'LineStyle','--','LineWidth',LW)
+%     ylabel('BIC');
+%     xlabel('Models');
+%     title(TITLE);
+%     axis([0 length(DEG_OF_FREEDOM)+2 min(min(aic),min(bic))-BAR_OFFSET 295]);
+%     xticks([]);
+%     set(gca,'FontSize',FS);
+%     hold off
 
 end
 
